35a9c498546f4c979133d6941ea93762e0111639
Mei Chock <59775096+shrimeichock@users.noreply.github.com>
2022-02-06 09:33:26 -0500

    Add areas that are less than 1 hectare
    Input images must be a 64 hectare square plot of land (800 x 800m)

--- detectColour.py
+++ detectColour.py
@@ -2,13 +2,14 @@
 """
 Created on Sat Feb  5 13:57:53 2022
 @author: user
+Source: https://techvidvan.com/tutorials/detect-objects-of-similar-color-using-opencv-in-python/ 
 """
 
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = cv2.imread('image.png')
+img = cv2.imread('OnePlot.png')
 
 #plt.imshow(img)
 grid_RGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -16,8 +17,8 @@ grid_RGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 grid_HSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 #plt.imshow(grid_HSV)
 
-lower_color = np.array([40, 25,0]) 
-upper_color = np.array([220, 225, 125])
+lower_color = np.array([50, 20, 0]) #increase H if more green in the picture
+upper_color = np.array([220, 225, 125]) #HueSaturationValue
 mask = cv2.inRange(grid_HSV, lower_color, upper_color)
 
 #define kernel size  
@@ -28,48 +29,26 @@ mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
 
 edged = cv2.Canny(mask, 170,255)
 contours, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-for cnt in contours:
-    moment=cv2.moments(cnt) 
-    #cx = int(moment['m10'] / moment['m00']) 
-    #cy = int(moment['m01'] / moment['m00']) 
-    #contoursNew, hierarchyNew = cv2.findContours(img.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    cv2.drawContours(img,[cnt],-1,(0,255,0),2)
+#output = cv2.drawContours(edged, contours, -1, (0, 0, 255), 3)
 
-#plt.imshow(img)
-
-
-def grab_contours(cnts):
-    # OpenCV v2.4, v4-official
-    if len(cnts) == 2:
-        return cnts[0]
-    # OpenCV v3
-    elif len(cnts) == 3:
-        return cnts[1]
-    
-#filename = "test.png"
-#cv2.imwrite(mask)
-
-image = cv2.imread("test.png")
-
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-edged = cv2.Canny(gray, 120, 255, 1)
-cv2.imshow("canny", edged)
-
-cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
-    cv2.CHAIN_APPROX_SIMPLE)
-cnts = grab_contours(cnts)
-
-contour_image = edged.copy()
 area = 0
+height, width, channels = img.shape
+print (height, width, channels)
+total_area = height*width
+hectare_size = total_area/64
 
-for c in cnts:
-    area += cv2.contourArea(c) 
-    cv2.drawContours(contour_image,[c], 0, (100,5,10), 3)
+for cnt in contours:
+    if cv2.contourArea(cnt) < hectare_size: #replace with # pixels for 1 hectare
+        area += cv2.contourArea(cnt)
+        cv2.drawContours(img,[cnt],-1,(0,255,0),2)
+    else:
+        cv2.drawContours(img,[cnt],-1,(200,0,0),2)
 
+percentage = area/total_area
 print(area)
-cv2.putText(contour_image, str(area), (50,50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (100,255,100), 2)
-cv2.imshow("area", contour_image)
-cv2.waitKey(0)
+print(percentage*100) 
+
+plt.imshow(img) #show green image
 
 segmented_img = cv2.bitwise_and(img, img, mask=mask)
 
@@ -78,6 +57,6 @@ segmented_img = cv2.bitwise_and(img, img, mask=mask)
 #cv2.imshow("Output", output)
 
 #plt.imshow(segmented_img)
-plt.imshow(mask)
+#plt.imshow(mask) #show purple image
 
 # cv2.imwrite('test-images/mask.png', mask)
