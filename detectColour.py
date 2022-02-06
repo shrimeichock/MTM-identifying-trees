814b4a596ceb423b300c72b59e127bb0e5bb8b7e
Mei Chock <59775096+shrimeichock@users.noreply.github.com>
2022-02-05 20:00:34 -0500

    Green outline algorithm (using drawContours)

--- detectColour.py
+++ detectColour.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Feb  5 13:57:53 2022
-
 @author: user
+Source: https://techvidvan.com/tutorials/detect-objects-of-similar-color-using-opencv-in-python/ 
 """
 
 import cv2
@@ -17,8 +17,8 @@ grid_RGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 grid_HSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 #plt.imshow(grid_HSV)
 
-lower_color = np.array([35, 25, 0]) 
-upper_color = np.array([220, 225, 125])
+lower_color = np.array([50, 20, 0]) #increase H if more green in the picture
+upper_color = np.array([220, 225, 125]) #HueSaturationValue
 mask = cv2.inRange(grid_HSV, lower_color, upper_color)
 
 #define kernel size  
@@ -29,10 +29,13 @@ mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
 
 edged = cv2.Canny(mask, 170,255)
 contours, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-output = cv2.drawContours(edged, contours, -1, (0, 0, 255), 3)
+#output = cv2.drawContours(edged, contours, -1, (0, 0, 255), 3)
 
+for cnt in contours:
+    moment=cv2.moments(cnt) 
+    cv2.drawContours(img,[cnt],-1,(0,255,0),2)
 
-
+plt.imshow(img) #show green image
 
 segmented_img = cv2.bitwise_and(img, img, mask=mask)
 
@@ -41,6 +44,6 @@ segmented_img = cv2.bitwise_and(img, img, mask=mask)
 #cv2.imshow("Output", output)
 
 #plt.imshow(segmented_img)
-plt.imshow(mask)
+#plt.imshow(mask) #show purple image
 
 # cv2.imwrite('test-images/mask.png', mask)
